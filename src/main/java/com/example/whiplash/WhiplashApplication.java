25185ac043e0eae9c72329ac1d6390c2a5717276
WithFortuna <rmsghchl0@naver.com>
2025-05-10 20:25:19 +0900

    init

---
+++ src/main/java/com/example/whiplash/WhiplashApplication.java
@@ -0,0 +1,13 @@
+package com.example.whiplash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiplashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiplashApplication.class, args);
+	}
+
+}
