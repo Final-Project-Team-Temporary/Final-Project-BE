d3d37d355eb4b97653ea7fd6f7b7f8cf39b71bda
GNO CHOI <rmsghchl0@naver.com>
2025-05-19 00:07:50 +0900

    [FEAT] 회원 CRUD 계층 구현
    - UserService 구현
     - UserRepository 구현
     - User 내부에서 update 처리

--- src/main/java/com/example/whiplash/user/User.java
+++ src/main/java/com/example/whiplash/user/User.java
@@ -1,31 +1,30 @@
-package com.example.whiplash.domain.user;
+package com.example.whiplash.user;
 
-import com.example.whiplash.domain.history.email.SummaryLevel;
+import com.example.whiplash.domain.entity.history.email.SummaryLevel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.management.relation.Role;
-
 import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
-@Entity
-@Table(name = "users")
 @Getter
+@Builder
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
-@Builder
+@Table(name = "users")
+@Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @Id @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     private String name;
+
     private Integer age;
+
     private String email;
 
     @Enumerated(STRING)
@@ -33,4 +32,12 @@ public class User {
 
     @Enumerated(STRING)
     private SummaryLevel summaryLevel;
+
+    public void update(UserModifyRequestDTO dto) {
+        this.name = dto.getName();
+        this.age = dto.getAge();
+        this.email = dto.getEmail();
+        this.summaryLevel = dto.getSummaryLevel();
+    }
 }
+
