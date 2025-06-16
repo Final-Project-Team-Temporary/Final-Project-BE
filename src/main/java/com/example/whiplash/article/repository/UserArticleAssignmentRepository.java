4997d3c90637ce403c9e5d1243619a821e38a590
GNO CHOI <rmsghchl0@naver.com>
2025-06-16 22:44:39 +0900

    [FEAT] UserArticleAssignment 상태 변경 메서드
    - 이메일 전송 후에 상태번경을 위한 업데이트 메서드 구현

--- src/main/java/com/example/whiplash/article/repository/UserArticleAssignmentRepository.java
+++ src/main/java/com/example/whiplash/article/repository/UserArticleAssignmentRepository.java
@@ -1,6 +1,7 @@
 package com.example.whiplash.article.repository;
 
 import com.example.whiplash.article.entity.UserArticleAssignment;
+import com.example.whiplash.domain.entity.history.email.EmailSendStatus;
 import com.example.whiplash.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface UserArticleAssignmentRepository extends JpaRepository<UserArticleAssignment, Long> {
     List<UserArticleAssignment> findAllByUser(User user);
+    List<UserArticleAssignment> findAllBySendStatus(EmailSendStatus sendStatus);
 }
