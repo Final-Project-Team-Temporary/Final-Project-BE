1f08cf0d33f21adfbe2ef372786c60d4d5d9c11a
WithFortuna <rmsghchl0@naver.com>
2025-08-18 21:20:24 +0900

    [REFACTOR] ArticleDeliveryOrchestrator 리팩토링

--- src/main/java/com/example/whiplash/global/HealthCheckController.java
+++ src/main/java/com/example/whiplash/global/HealthCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @GetMapping("/my-health")
+    @GetMapping("/my-health/good")
     public String healthCheck() {
         return "OK";
     }
