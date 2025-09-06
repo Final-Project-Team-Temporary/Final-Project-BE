450be696ed0e0198738304d22a4c60122aa43c40
WithFortuna <rmsghchl0@naver.com>
2025-09-06 20:48:50 +0900

    [REFACTOR] Jpa EnableAuditing config 분리

--- src/main/java/com/example/whiplash/WhiplashApplication.java
+++ src/main/java/com/example/whiplash/WhiplashApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableJpaAuditing
 public class WhiplashApplication {
 
     public static void main(String[] args) {
