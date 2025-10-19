abe2f934312da95a9d8288db6a95cedd72b8dd11
WithFortuna <rmsghchl0@naver.com>
2025-10-19 22:06:18 +0900

    [FIX] Redis tls 접속

--- src/main/java/com/example/whiplash/global/config/RedisConfig.java
+++ src/main/java/com/example/whiplash/global/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -13,9 +15,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory(
-            @Value("${spring.data.redis.host}") String host,
-            @Value("${spring.data.redis.port}") int port) {
-        return new LettuceConnectionFactory(host, port);
+        @Value("${spring.data.redis.host}") String host,
+        @Value("${spring.data.redis.port}") int port,
+        @Value("${spring.data.redis.ssl.enabled:true}") boolean sslEnabled) {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder clientConfig =
+            LettuceClientConfiguration.builder();
+
+        if (sslEnabled) {
+            // TLS 활성화하되, 인증서 검증은 건너뜀 (VPC 내부라 안전)
+            clientConfig.useSsl().disablePeerVerification();
+        }
+
+        return new LettuceConnectionFactory(config, clientConfig.build());
     }
 
     @Bean
