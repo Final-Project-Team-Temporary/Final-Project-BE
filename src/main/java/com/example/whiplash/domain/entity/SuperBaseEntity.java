6dbaabca5e83bc17bfce5e7dd2052525374e26a2
WithFortuna <rmsghchl0@naver.com>
2025-11-26 22:19:05 +0900

    [FIX] @SuperBuilder가 필요한 경우 별도의 base entity를 가지도록 변경

--- src/main/java/com/example/whiplash/domain/entity/SuperBaseEntity.java
+++ src/main/java/com/example/whiplash/domain/entity/SuperBaseEntity.java
@@ -1,26 +1,27 @@
 package com.example.whiplash.domain.entity;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @MappedSuperclass // JPA에서 상속받는 클래스의 공통 필드들을 테이블 컬럼으로 인식하게 해주는 어노테이션
 @Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
+public class SuperBaseEntity {
+	@CreatedDate
+	private LocalDateTime createdAt;
 
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
 }
